e4846716a36b1c9648bd715d71884e8e4443b070
Eldin Zaimovic <33357696+EldinZaimovic@users.noreply.github.com>
2019-09-06 15:03:37 +0200

    Update html.js

--- src/html.js
+++ src/html.js
@@ -1,5 +1,7 @@
 import React from "react"
 import PropTypes from "prop-types"
+import favicon from './static/images/favicon.png';
+<link rel="shortcut icon" type="image/png" href={favicon} />
 
 export default function HTML(props) {
   return (
